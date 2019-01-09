9c5bcdede40167534e5efcb9df2935afdb26651e
rdaley321 <rdaley321@yahoo.com>
2019-01-08 20:41:46 -0600

    reduce working

--- index.js
+++ index.js
@@ -4,18 +4,28 @@ fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      for (let index in collection) {
+        callback(collection[index]);
+      }
+      return collection;
     },
 
-    map: function() {
-
+    map: function(collection, callback) {
+      let newArray = []
+      for (let index in collection) {
+        newArray.push(callback(collection[index]));
+      }
+      return newArray;
     },
 
-    reduce: function() {
-
+    reduce: function(collection, callback, acc = 0) {
+      for (var i in collection) {
+        acc = callback(acc, collection[i], collection)
+      };
+      return acc;
     },
-    
+
     functions: function() {
 
     },
